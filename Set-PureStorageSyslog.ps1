374e53c6d08ea121559e6c325a1cb62d5ba79668
David Stamen <dstamen@gmail.com>
2016-03-14 09:25:23 -0400

    post

---
+++ Set-PureStorageSyslog.ps1
@@ -0,0 +1,15 @@
+# PureStorage Powershell SDK Script to set Syslog Server
+# @davidstamen
+# http://davidstamen.com
+
+#Define Variables
+$cred = Get-Credential
+$arrays = "pure.lab.local"
+$SysLogServers = "tcp://syslog.lab.local"
+
+#Cycles through each UCS setting values
+Foreach ($array in $arrays) {
+  Write-Host "Getting Syslog Information for $array"
+  $FlashArray = New-PfaArray -EndPoint $array -Credentials $cred -IgnoreCertificateError
+  Set-PfaSyslogServers -Array $FlashArray -SyslogServers $SyslogServers|fl
+}
