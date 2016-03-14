4d156557a158bc8cf4f0fc46dda803fefabdd966
David Stamen <dstamen@gmail.com>
2016-03-14 09:36:19 -0400

    update

--- Set-PureStorageSyslog.ps1
+++ Set-PureStorageSyslog.ps1
@@ -2,10 +2,13 @@
 # @davidstamen
 # http://davidstamen.com
 
+#Import Module
+Import-Module PureStoragePowershellSDK
+
 #Define Variables
 $cred = Get-Credential
 $arrays = "pure.lab.local"
-$SysLogServers = "tcp://syslog.lab.local"
+$SysLogServers = "tcp://syslog.lab.local:514"
 
 #Cycles through each UCS setting values
 Foreach ($array in $arrays) {
